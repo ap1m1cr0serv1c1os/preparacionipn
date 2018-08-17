81924866036154d1b3e0982b1106f9c6137e491b
ap1m1cr0serv1c1os <42472819+ap1m1cr0serv1c1os@users.noreply.github.com>
2018-08-17 09:53:45 -0500

    inicio de proyecto

---
+++ preparacionipn/src/main/groovy/com/preparacionipn/preparacionipn/PreparacionipnApplication.groovy
@@ -0,0 +1,12 @@
+package com.preparacionipn.preparacionipn
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PreparacionipnApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run PreparacionipnApplication, args
+	}
+}
---
+++ preparacionipn/src/test/groovy/com/preparacionipn/preparacionipn/PreparacionipnApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.preparacionipn.preparacionipn
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class PreparacionipnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
