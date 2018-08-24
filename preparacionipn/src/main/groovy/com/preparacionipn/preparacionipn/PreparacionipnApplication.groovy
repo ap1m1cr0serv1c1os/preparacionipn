f6b086ea898b6c8f2370eab4318c287667601ecc
alfoncovaco <alfonso.vasquez@apimicroservicios.com>
2018-08-24 17:04:42 -0500

    Configurando el direccionamiento de errores

--- preparacionipn/src/main/groovy/com/preparacionipn/preparacionipn/PreparacionipnApplication.groovy
+++ preparacionipn/src/main/groovy/com/preparacionipn/preparacionipn/PreparacionipnApplication.groovy
@@ -2,8 +2,10 @@ package com.preparacionipn.preparacionipn
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class PreparacionipnApplication {
 
 	static void main(String[] args) {
