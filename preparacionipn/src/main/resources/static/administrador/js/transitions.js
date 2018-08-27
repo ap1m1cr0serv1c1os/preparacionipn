099642f683859990e4bca306d26aedeb60e5569b
alfoncovaco <alfonso.vasquez@apimicroservicios.com>
2018-08-27 10:54:51 -0500

    Eliminando clase has sub la cual especifica contenedores en menus

--- preparacionipn/src/main/resources/static/administrador/js/transitions.js
+++ preparacionipn/src/main/resources/static/administrador/js/transitions.js
@@ -2,9 +2,8 @@ function getItem(id) {
 	var hrefs = $(id).attr("data-href-to-view");
 	if ($(id).attr("data-href-to-menu") != undefined) {
 		var contenido = $(id).attr("data-href-to-menu");
-		$(".change-menu").find(".has-sub").removeClass("has-sub");
 		$(".change-menu").find(".active").removeClass("active");
-		$(".change-menu li span[data-href*='" + contenido + "']").parent("li").addClass("active has-sub");
+		$(".change-menu li span[data-href*='" + contenido + "']").parent("li").addClass("active");
 		document.title = contenido;
 	}
 
