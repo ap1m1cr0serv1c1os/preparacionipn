f2f2c56e196aeddf0d214e14501a05a557e94212
alfoncovaco <alfonso.vasquez@apimicroservicios.com>
2018-09-03 16:33:25 -0500

    Se editó la cookie que no actualiza al primer click

--- preparacionipn/src/main/resources/static/alumno/js/cookies.js
+++ preparacionipn/src/main/resources/static/alumno/js/cookies.js
@@ -48,6 +48,8 @@ $(document).ready(function(){
 		}else{
 			$(".cart").text("1");
 			document.cookie = "_lstproduct=" + idProduct + ";" + getExpiresCookie() + ";path=/";
+			$(this).prop( "disabled", true );
+			$(this).text("Agregado");
 		}
 		
 		let image=new Element('div',{class:'modal-window-image'},["<svg viewBox=\"0 0 32 32\" style=\"fill:#48DB71\"><path d=\"M1 14 L5 10 L13 18 L27 4 L31 8 L13 26 z\"></path></svg>"]);
